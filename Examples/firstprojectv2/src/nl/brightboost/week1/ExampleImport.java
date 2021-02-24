f93e703cfc8d5eb1b47310896d62f7518b2a4cf3
RenatevanKempen <renate@baime.nl>
2021-02-24 13:53:41 +0100

    Create ExampleImport.java

---
+++ Examples/firstprojectv2/src/nl/brightboost/week1/ExampleImport.java
@@ -0,0 +1,10 @@
+package nl.brightboost.week1;
+
+import java.time.LocalDate; //automatically import if you use this package in your class
+import java.time.*; // imports all within java.time
+
+public class ExampleImport {
+    public static void main(String[] args) {
+        LocalDate ld = LocalDate.now();
+    }
+}
