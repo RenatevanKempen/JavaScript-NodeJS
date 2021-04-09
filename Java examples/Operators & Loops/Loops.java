7b00fbfb9a7edf479b0b373c9674f427a932e850
RenatevanKempen <renate@baime.nl>
2021-04-09 08:31:43 +0200

    Create Loops.java

---
+++ Java examples/Operators & Loops/Loops.java
@@ -0,0 +1,27 @@
+package nl.brightboost.week3;
+
+public class Loops {
+    public static void main(String[] args) {
+        //for loop
+        //    for(int i = 0; i< 10; i++){System.out.println(i + " iteration");
+        //    }
+        //enhanced for loop
+        //       String[] stringsx = {"hi", "hello", "bye"};
+        //       for(String s: stringsx) {
+        //           System.out.println(s);
+//        }
+        //while
+//        int i = 0;
+//        while (i<15) {
+//            System.out.println(i);
+//            i++;
+//        }
+        //do while
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 0);
+
+    }
+}
