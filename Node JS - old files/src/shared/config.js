34d623afd3edeed228dfdd040b379f8d185a02f6
RenatevanKempen <renate@baime.nl>
2021-05-11 08:44:35 +0200

    Create config.js

---
+++ Node JS - old files/src/shared/config.js
@@ -0,0 +1,11 @@
+// @flow
+
+import React from "react";
+export const WEB_PORT = process.env.PORT || 8000
+export const WDS_PORT = 7000
+export const STATIC_PATH = "/static"
+
+export const APP_NAME = "Why doesn't the original work properly??"
+
+export const APP_CONTAINER_CLASS = 'js-app'
+export const APP_CONTAINER_SELECTOR = `.${APP_CONTAINER_CLASS}`
